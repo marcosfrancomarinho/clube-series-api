d75c21036debaba68ac00eb872304dbdce1bd53d
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-12-21 18:52:20 -0300

    'finalizado'

--- dist/routers/signup-router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const instances_1 = require("../config/instances");
-const routerRegister = (0, express_1.Router)();
-routerRegister.post('/', instances_1.registerControllers.siginUpUser);
-exports.default = routerRegister;

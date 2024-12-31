ce01fa30b8ad68acde502c0ab885879507f759e7
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-12-31 19:31:11 -0300

    '31/12/2024'

--- dist/model/Structure/structure-footer/structure-footer.js
+++ dist/model/Structure/structure-footer/structure-footer.js
@@ -1,6 +1,6 @@
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+    return (mod && mod.__esModule) ? mod : { "default": mod }; 
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
@@ -12,6 +12,7 @@ class StructureFooter extends sequelize_1.Model {
     createdAt;
     updatedAt;
 }
+console.log('teste')
 StructureFooter.init({
     id: {
         primaryKey: true,

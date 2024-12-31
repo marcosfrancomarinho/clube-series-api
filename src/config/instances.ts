87dbcd0e22127ba65301e7cb57c73140ef6820b3
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-12-31 19:19:51 -0300

    'refatoracao'

--- src/config/instances.ts
+++ src/config/instances.ts
@@ -1,29 +1,43 @@
 import jwt from 'jsonwebtoken';
 import Joi from 'joi';
 import bcrypt from 'bcrypt';
+
+// Modelos
 import User from '../model/user/user';
-import GenerateHash from '../util/generate-hash/generate-hash';
-import Encrypt from '../util/encrypt/encrypt';
-import VerifyDatasUser from '../util/verify-datas/verify-datas-user';
 import StructureFooter from '../model/structure/structure-footer/structure-footer';
 import StructureImages from '../model/structure/structure-images/structure-images';
 import StructureMenu from '../model/structure/structure-menu/structure-menu';
+
+// Utilitários
+import GenerateHash from '../util/generate-hash/generate-hash';
+import Encrypt from '../util/encrypt/encrypt';
+import VerifyDatasUser from '../util/verify-datas/verify-datas-user';
+
+// Middlewares
 import VerifyLogin from '../middlewares/verify-login/verify-login';
 import VerifyRegister from '../middlewares/verify-register/verify-register';
 import AuthenticateUser from '../middlewares/authenticate-user/authenticate-user';
+
+// Adaptadores
 import LoginAdapter from '../integrations/login-adapter/login-adapter';
 import RegisterAdapter from '../integrations/register-adapter/register-adapter';
 import StructureSelectAdapter from '../integrations/structure-adapter/structure-select-adapter/structure-select-adapter';
+
+// Serviços
 import LoginDbUserService from '../service/login-db-user-service/login-db-user-service';
 import RegisterDbUserService from '../service/register-db-user-service/register-db-user-service';
+import StructureDbSelectService from '../service/structure-db-select-service/structure-db-select-service';
+
+// Controladores
 import LoginControllers from '../controllers/login-controllers/login-controllers';
 import RegisterControllers from '../controllers/register-controllers/register-controllers';
 import WelcomeControllers from '../controllers/welcome-controllers/welcome-controllers';
-import StructureDbSelectService from '../service/structure-db-select-service/structure-db-select-service';
 import PageInterfaceControllers from '../controllers/page-interface-controllers/page-interface-controllers';
+
+// Configuração adicional
 import { option_query_select_attributes_db } from './option-query-select-attributes-db';
 
-// Adaptadores para acessar os dados das estruturas
+// Adaptadores de estruturas
 const structureSelectAdapterFooter = new StructureSelectAdapter(
 	StructureFooter,
 );
@@ -32,46 +46,46 @@ const structureSelectAdapterImages = new StructureSelectAdapter(
 );
 const structureSelectAdapterMenu = new StructureSelectAdapter(StructureMenu);
 
-// Serviço para gerenciar seleção no banco de dados
+// Serviço de seleção no banco de dados
 const structureDbSelectService = new StructureDbSelectService(
 	structureSelectAdapterFooter,
 	structureSelectAdapterImages,
 	structureSelectAdapterMenu,
 );
 
-// Controlador para gerenciar a interface das páginas
+// Controlador de interface de páginas
 const pageInterfaceControllers = new PageInterfaceControllers(
 	structureDbSelectService,
 	option_query_select_attributes_db,
 );
 
-// Utilitários para autenticação e encriptação
-const generateHash = new GenerateHash(jwt); // Geração e verificação de tokens JWT
-const encrypt = new Encrypt(bcrypt); // Encriptação e verificação de senhas
-const verifyDataUser = new VerifyDatasUser(Joi); // Validação de dados do usuário
+// Instâncias de utilitários
+const generateHash = new GenerateHash(jwt);
+const encrypt = new Encrypt(bcrypt);
+const verifyDataUser = new VerifyDatasUser(Joi);
 
 // Middlewares
-const verifyLogin = new VerifyLogin(verifyDataUser); // Validação de dados de login
-const verifyRegister = new VerifyRegister(verifyDataUser); // Validação de dados de registro
-const authenticateUser = new AuthenticateUser(generateHash); // Autenticação via token JWT
+const verifyLogin = new VerifyLogin(verifyDataUser);
+const verifyRegister = new VerifyRegister(verifyDataUser);
+const authenticateUser = new AuthenticateUser(generateHash);
 
-// Adaptadores de banco de dados para login e registro
+// Adaptadores de banco de dados
 const loginAdapter = new LoginAdapter(User);
 const registerAdapter = new RegisterAdapter(User);
 
-// Serviços de login e registro
-const loginUserDbService = new LoginDbUserService(encrypt, loginAdapter); // Serviço de login
+// Serviços
+const loginUserDbService = new LoginDbUserService(encrypt, loginAdapter);
 const registerUserDbService = new RegisterDbUserService(
 	encrypt,
 	registerAdapter,
-); // Serviço de registro
+);
 
 // Controladores
-const loginControllers = new LoginControllers(loginUserDbService, generateHash); // Controlador de login
-const registerControllers = new RegisterControllers(registerUserDbService); // Controlador de registro
-const welcomeControllers = new WelcomeControllers(); // Controlador de boas-vindas
+const loginControllers = new LoginControllers(loginUserDbService, generateHash);
+const registerControllers = new RegisterControllers(registerUserDbService);
+const welcomeControllers = new WelcomeControllers();
 
-// Exportação das instâncias
+// Exportações
 export {
 	verifyLogin,
 	verifyRegister,
--- src/service/structure-db-select-service/structure-db-select-service.ts
+++ src/service/structure-db-select-service/structure-db-select-service.ts
@@ -22,6 +22,9 @@ class StructureDbSelectService<F, M, I> implements IStructureDbSelectService {
 				this.structureAdapterSelectImages.search(attrImages),
 				this.structureAdapterSelectMenu.search(attrMenu),
 			]);
+			console.log((footer as unknown as Array<string>).length)
+			console.log((images as unknown as Array<string>).length)
+			console.log((menu as unknown as Array<string>).length)
 			return {
 				footer,
 				images,

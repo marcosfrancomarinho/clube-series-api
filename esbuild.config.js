4694a0279c0872214602bef5c906d9d886a65ea4
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2025-01-26 00:41:56 -0300

    '25/01/2025'

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,8 +3,8 @@ const esbuild = require("esbuild");
 esbuild
 	.build({
 		entryPoints: ["./src/app.ts"],
-		outfile: "./dist/app.js",
-		bundle: false,
+		outfile: "./dist/bundle.js",
+		bundle: true,
 		minify: true,
 		platform: "node",
 		external: ["bcrypt", "cors", "express", "joi", "jsonwebtoken", "pg"],
@@ -13,4 +13,4 @@ esbuild
 		minifyIdentifiers: true,
 		target: "es6",
 	})
-	.catch(() => process.exit(1));
+	.catch(() => process.exit(1))

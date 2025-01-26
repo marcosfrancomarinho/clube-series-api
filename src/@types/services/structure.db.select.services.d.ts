c312827247053b3fbdf1a075589ee8b0205cec41
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2025-01-26 19:16:39 -0300

    finalizado 3

--- src/@types/services/structure.db.select.services.d.ts
+++ src/@types/services/structure.db.select.services.d.ts
@@ -3,7 +3,7 @@ import { IStructureFooter, IStructureImages, IStructureMenu } from "../model/str
 
 export type IAttribute = { name: string; list: string[]; table: string };
 export type IAttributesSelectDb = { footer: IAttribute; images: IAttribute; menu: IAttribute };
-export type ISelectServicesRequest = [IStructureFooter[], IStructureImages[], IStructureMenu[]];
+export type ISelectServicesRequest = [IStructureFooter[] | IStructureImages[] | IStructureMenu[]];
 export type ISelectServicesResponse = { footer: IStructureFooter[]; images: IStructureImages[]; menu: IStructureMenu[] };
 
 export interface IStructureDbSelectServices {

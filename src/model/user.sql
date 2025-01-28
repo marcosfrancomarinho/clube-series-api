9d980105169d1bad872909bd42657ca98aea60ec
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2025-01-27 22:00:50 -0300

    'chega'

--- src/model/user.sql
+++ src/model/user.sql
@@ -5,15 +5,4 @@ CREATE TABLE register_user (
     password VARCHAR NOT NULL
 );
 
-ALTER TABLE register_user
-ALTER COLUMN name TYPE VARCHAR(255),
-ALTER COLUMN name SET NOT NULL;
-
-ALTER TABLE register_user
-ALTER COLUMN email TYPE VARCHAR(255),
-ALTER COLUMN email SET NOT NULL;
-
-ALTER TABLE register_user
-ADD CONSTRAINT unique_email UNIQUE (email);
-
 
